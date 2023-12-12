7502741963f299cc267a0f05ea3467a1eda77ca1
FabienArthurYnov <fabien.arthur@ynov.com>
2023-12-12 09:16:16 +0100

    get by id working for address

--- Controllers/AddressConstroller.cs
+++ Controllers/AddressConstroller.cs
@@ -7,6 +7,9 @@ namespace Controllers;
 class AddressController : Controller
 {
     public override void GetRequest(HttpListenerResponse response, string content = "", int statusCode = 501) {
+        // status ok if not overriden by unexpected events
+        statusCode = 200;
+
         // will hold the addresses
         List<Address> addresses = new List<Address>();
 
@@ -42,19 +45,63 @@ class AddressController : Controller
             Console.WriteLine("Error : " + e.Message);
             transaction.Rollback();
             Console.WriteLine("Transaction rolled back");
+            // internal error
+            statusCode = 500;
         }
 
 
         // to json
         string jsonAddresses = JsonSerializer.Serialize(addresses);
-        Console.WriteLine(jsonAddresses);
 
-        // change content and statusCode here
-        statusCode = 200;
+        // change content
         content = jsonAddresses;
 
         base.GetRequest(response, content, statusCode); // base keyword : like super() in java, call the parent class
     }
 
+
+    public override void GetByIdRequest(HttpListenerResponse response, int id, string content = "", int statusCode = 501) {
+        statusCode = 200;
+        Address address = new Address();
+
+        MySqlConnection connection = new MySqlConnection(ApiServer.ConnectionString);
+        connection.Open();
+        
+        MySqlTransaction transaction = connection.BeginTransaction();
+        try {
+            // the command as a string with correct id
+            string commandString = "SELECT * FROM Addresses WHERE AddressId = " + id.ToString() + ";";
+            MySqlCommand command = new MySqlCommand(commandString, connection, transaction);
+            MySqlDataReader reader = command.ExecuteReader();
+
+            if (reader.HasRows) {
+                reader.Read();
+                address = new Address
+                {
+                    AddressId = reader.GetInt32(0),
+                    UserId = reader.GetInt32(1),
+                    AddressString = reader.GetString(2)
+                };
+
+                //check if there is another row
+                if (reader.Read()) {
+                    Console.WriteLine("Warning ; multiple result on a GET by Id in Address  (id:"+id.ToString()+")");
+                }
+            }
+            reader.Close();
+            transaction.Commit();
+        } catch (Exception e) {
+            Console.WriteLine("Error : " + e.Message);
+            transaction.Rollback();
+            Console.WriteLine("Transaction rolled back");
+            statusCode = 500;
+        }
+
+        string jsonAddresses = JsonSerializer.Serialize(address);
+
+        content = jsonAddresses;
+
+        base.GetRequest(response, content, statusCode);
+    }
 
 }
